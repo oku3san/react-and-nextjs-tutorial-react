5b5edadc2f57a6016c17834435039685cf10cf19
oku3san <takaya0303@gmail.com>
2021-09-20 23:34:33 +0900

    Add child state

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import Rect from './Rect';
 
-function App(props: any) {
-
-  const [msg, setMsg] = useState('Count start')
-  const [count, setCount] = useState(0)
-
-  // setInterval(() => {
-  //   setCount(count + 1)
-  //   setMsg("[count: " + count + " ]")
-  // }, 1000)
-
-  const doAction = () => {
-    setCount(count + 1)
-    setMsg("[count: " + count + " ]")
-  }
+const App = (props: any) => {
+  let input = ''
 
   return (
-    <div>
+    <>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">Count number</p>
-        <p className="alert alert-warning">{msg}</p>
-        <button onClick={() => doAction()}>
-          Click me
-        </button>
+        <Message title="Children">
+          これはコンポーネント内のコンテンツです。
+          丸でテキストを分割し、リストにて表示します。
+          改行は必要ありません。
+        </Message>
       </div>
-    </div>
-  );
+    </>
+  )
+}
+
+const Message = (props: any) => {
+  const li: any = {
+    fontSize: "14pt",
+    fontWeight: "bold",
+    color: "#090",
+  }
+
+  let content = props.children
+  let arr = content.split('。')
+  let arr2 = []
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i].trim() != '') {
+      arr2.push(arr[i])
+    }
+  }
+  let list = arr2.map((value, key) => {
+    return <li className="list-group-item" style={li} key={key}>{key + 1}. {value}</li>
+  })
+
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <ol className="list-group">{list}</ol>
+    </>
+  )
 }
 
 export default App;
