2a2e9dfb089a3f4f6b9102e270d72710ad3dfeb4
oku3san <takaya0303@gmail.com>
2021-09-20 23:04:54 +0900

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Rect from './Rect';
 
 function App(props: any) {
+
+  const [msg, setMsg] = useState('Count start')
+  const [count, setCount] = useState(0)
+
+  // setInterval(() => {
+  //   setCount(count + 1)
+  //   setMsg("[count: " + count + " ]")
+  // }, 1000)
+
+  const doAction = () => {
+    setCount(count + 1)
+    setMsg("[count: " + count + " ]")
+  }
+
   return (
-    <div className="App">
+    <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">draw rectangle</p>
-        <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-        <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-        <Rect x="400" y="400" w="200" h="200" c="#66f9" r="100" />
-        <Rect />
+        <p className="subtitle">Count number</p>
+        <p className="alert alert-warning">{msg}</p>
+        <button onClick={() => doAction()}>
+          Click me
+        </button>
       </div>
     </div>
   );
