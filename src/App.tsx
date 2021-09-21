3d1b9f11a765ef590466dbd6c6acc33218c2db10
oku3san <takaya0303@gmail.com>
2021-09-22 00:46:26 +0900

    4-2 hook function 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-const AlertMessage = () => {
+const AlertMessage = (props: any) => {
   return (
     <div className="alert alert-primary h5 text-primary">
-      This is Alert message
+      {props.message}
     </div>
   )
 }
 
-const CardMessage = () => {
+const CardMessage = (props: any) => {
   return (
     <div className="card p-3 h5 border-primary text-center">
-      This is Card message
+      {props.message}
     </div>
   )
 }
 
 const App = (props: any) => {
 
+  const [msg] = useState('aiueo')
+
   return (
     <>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hook sample</h4>
-        <AlertMessage />
-        <CardMessage />
+        <AlertMessage message={msg} />
+        <CardMessage message={msg} />
       </div>
     </>
   )
