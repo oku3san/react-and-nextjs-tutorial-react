9eddcf5758a89fc42a272a07f7fc2b1f7737f357
oku3san <takaya0303@gmail.com>
2021-09-22 00:38:06 +0900

    4-2 hook function

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,31 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Rect from './Rect';
+
+const AlertMessage = () => {
+  return (
+    <div className="alert alert-primary h5 text-primary">
+      This is Alert message
+    </div>
+  )
+}
+
+const CardMessage = () => {
+  return (
+    <div className="card p-3 h5 border-primary text-center">
+      This is Card message
+    </div>
+  )
+}
 
 const App = (props: any) => {
-
-  const [count, setCount] = useState(0)
-  const [flag, setFlag] = useState(false)
-
-  const clickFunc = () => {
-    setCount(count + 1)
-  }
-
-  const changeFlag = (event: any) => {
-    setFlag(event.target.checked)
-  }
 
   return (
     <>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4 className="my-3">Hooks sample</h4>
-        {flag ?
-          <div className="alert alert-primary text-center">
-            <p className="h5">click: {count} times</p>
-            <div>
-              <button className="btn btn-primary" onClick={clickFunc}>Click me</button>
-            </div>
-          </div>
-        :
-          <div className="alert alert-primary text-center">
-            <p className="h2">click: {count} times</p>
-            <div>
-              <button className="btn btn-primary" onClick={clickFunc}>Click me</button>
-            </div>
-          </div>
-        }
-        <div className="form-group h6 text-center pt-3">
-          <input type="checkbox" className="form-check-input" id="check1" onChange={changeFlag} />
-          <label className="form-check-label" htmlFor="check1">
-            Change form style
-          </label>
-        </div>
+        <h4 className="my-3">Hook sample</h4>
+        <AlertMessage />
+        <CardMessage />
       </div>
     </>
   )
