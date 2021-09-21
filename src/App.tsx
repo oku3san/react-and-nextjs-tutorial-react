81fecdb68f452038e91fcd68660d701ddb2d6820
oku3san <takaya0303@gmail.com>
2021-09-22 00:26:08 +0900

    4-1 hook and flag

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Rect from './Rect';
 
-let data = {
-  title: 'React-Context',
-  message: 'this is sample message'
-}
-
-const SampleContext = React.createContext(data)
-
 const App = (props: any) => {
 
-  let newdata = {
-    title: 'new React-Context',
-    message: 'new this is sample message'
+  const [count, setCount] = useState(0)
+  const [flag, setFlag] = useState(false)
+
+  const clickFunc = () => {
+    setCount(count + 1)
+  }
+
+  const changeFlag = (event: any) => {
+    setFlag(event.target.checked)
   }
 
   return (
     <>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <Title />
-        <Message />
-        <hr />
-        <SampleContext.Provider value={newdata}>
-          <Title />
-          <Message />
-        </SampleContext.Provider>
-        <hr />
-        <Title />
-        <Message />
-      </div>
-    </>
-  )
-}
-
-const Title = (props: any) => {
-  const data: any = React.useContext(SampleContext)
-
-  return (
-    <>
-      <div className="card p-2 my-3">
-        <h2>{data.title}</h2>
-      </div>
-    </>
-  )
-}
-
-const Message = (props: any) => {
-  const data: any = React.useContext(SampleContext)
-
-  return (
-    <>
-      <div className="alert alert-primary">
-        <p>{data.message}</p>
+        <h4 className="my-3">Hooks sample</h4>
+        {flag ?
+          <div className="alert alert-primary text-center">
+            <p className="h5">click: {count} times</p>
+            <div>
+              <button className="btn btn-primary" onClick={clickFunc}>Click me</button>
+            </div>
+          </div>
+        :
+          <div className="alert alert-primary text-center">
+            <p className="h2">click: {count} times</p>
+            <div>
+              <button className="btn btn-primary" onClick={clickFunc}>Click me</button>
+            </div>
+          </div>
+        }
+        <div className="form-group h6 text-center pt-3">
+          <input type="checkbox" className="form-check-input" id="check1" onChange={changeFlag} />
+          <label className="form-check-label" htmlFor="check1">
+            Change form style
+          </label>
+        </div>
       </div>
     </>
   )
