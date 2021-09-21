a61e25ee6be91a5520a5e0fb765331eca9db8938
oku3san <takaya0303@gmail.com>
2021-09-21 23:36:45 +0900

    3-4 useContext provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,63 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Rect from './Rect';
 
+let data = {
+  title: 'React-Context',
+  message: 'this is sample message'
+}
+
+const SampleContext = React.createContext(data)
+
 const App = (props: any) => {
-  let input = ''
 
-  const [title, setTitle] = useState('input form')
-  const [message, setMessage] = useState('type your name')
-  const [max, setMax] = useState(10)
-
-  const doCheck = (event: any) => {
-    alert(`${event.target.value} は長すぎます。(最大 ${max} 文字)`)
+  let newdata = {
+    title: 'new React-Context',
+    message: 'new this is sample message'
   }
 
   return (
     <>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4>{title}</h4>
-        <Message maxlength={max} onCheck={doCheck} />
+        <Title />
+        <Message />
+        <hr />
+        <SampleContext.Provider value={newdata}>
+          <Title />
+          <Message />
+        </SampleContext.Provider>
+        <hr />
+        <Title />
+        <Message />
+      </div>
+    </>
+  )
+}
+
+const Title = (props: any) => {
+  const data: any = React.useContext(SampleContext)
+
+  return (
+    <>
+      <div className="card p-2 my-3">
+        <h2>{data.title}</h2>
       </div>
     </>
   )
 }
 
 const Message = (props: any) => {
-
-  const li: any = {
-    fontSize: "14pt",
-    fontWeight: "bold",
-    color: "#090",
-  }
-
-  const doChange = (event: any) => {
-    if (event.target.value.length > props.maxlength) {
-      props.onCheck(event)
-      event.target.value.substr(0, props.maxlength)
-    }
-  }
+  const data: any = React.useContext(SampleContext)
 
   return (
     <>
-      <div className="form-group">
-        <label>input:</label>
-        <input type="text" className="form-control" onChange={doChange} />
+      <div className="alert alert-primary">
+        <p>{data.message}</p>
       </div>
     </>
   )
-
 }
 
 export default App;
