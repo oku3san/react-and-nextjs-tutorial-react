f085e5f475b04f6e00f9d3b89d9c1c67e89297f2
oku3san <takaya0303@gmail.com>
2021-09-23 22:40:27 +0900

    4-3 side-effect initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const AlertMessage = (props: any) => {
-  const data = props.data
-  const msg = JSON.stringify(data)
-
   return (
     <>
       <div className="alert alert-primary h5 text-primary">
-        <h5>{msg}</h5>
-        <hr/>
-        <table className="table h6">
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <td>{data.name}</td>
-            </tr>
-            <tr>
-              <th>Mail</th>
-              <td>{data.mail}</td>
-            </tr>
-            <tr>
-              <th>Age</th>
-              <td>{data.age}</td>
-            </tr>
-          </tbody>
-        </table>
+        <h5>{props.msg}</h5>
       </div>
     </>
   )
@@ -33,51 +13,31 @@ const AlertMessage = (props: any) => {
 
 const App = (props: any) => {
 
-  const [name, setName] = useState('')
-  const [mail, setMail] = useState('')
-  const [age, setAge] = useState(0)
-  const [form, setForm] = useState({
-    name: 'no name', mail: 'no mail', age: 0
+  const [val, setVal] = useState(0)
+  const [msg, setMsg] = useState('set a number...')
+
+  const doChange = (event: any) => {
+    setVal(event.target.value)
+  }
+
+  useEffect(() => {
+    let total = 0
+    for (let i = 0; i <= val; i++) {
+      total += i
+    }
+    setMsg("Total:" + total + ".")
   })
-
-  const doChangeName = (event: any) => {
-    setName(event.target.value)
-  }
-
-  const doChangeMail = (event: any) => {
-    setMail(event.target.value)
-  }
-
-  const doChangeAge = (event: any) => {
-    setAge(event.target.value)
-  }
-
-  const doSubmit = (event: any) => {
-    setForm({name: name, mail: mail, age: age})
-    event.preventDefault()
-  }
 
   return (
     <>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hook sample</h4>
-        <AlertMessage data={form} setData={setForm} />
-        <form onSubmit={doSubmit}>
-          <div className="form-group">
-            <label>Name:</label>
-            <input type="text" className="form-control" onChange={doChangeName} />
-          </div>
-          <div className="form-group">
-            <label>Mail:</label>
-            <input type="text" className="form-control" onChange={doChangeMail} />
-          </div>
-          <div className="form-group">
-            <label>Age:</label>
-            <input type="text" className="form-control" onChange={doChangeAge} />
-          </div>
-          <input type="submit" className="btn btn-primary" value="Click" />
-        </form>
+        <AlertMessage msg={msg} />
+        <div className="form-grou">
+          <label>Input:</label>
+          <input type="number" className="form-control" onChange={doChange} />
+        </div>
       </div>
     </>
   )
